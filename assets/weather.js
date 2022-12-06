4cd0e35ec4b451749b351985a23b7d4be102b714
Blake <blakepetersont@gmail.com>
2022-12-06 16:58:59 -0700

    finished adding the code to get the weather data on the page

--- assets/weather.js
+++ assets/weather.js
@@ -1,6 +1,6 @@
 var apiKey = "b711981b9768551b14c4803eeb3cca40"
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=london&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?q=london&cnt=3&appid=${apiKey}`)
   .then((response) => response.json())
   .then((data) => console.log(data));
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
@@ -63,8 +63,6 @@ console.log(lowTemp)
 let wind = resp[1].wind.speed
 let humidity =  resp[1].main.humidity
 console.log(humidity)
-let rain = resp[1].weather.rain
-console.log(rain)
 let secondTemp = resp[0].list[5].main.temp
 console.log(secondTemp)
 let secondHigh = resp[0].list[5].main.temp_max
@@ -72,6 +70,21 @@ let secondLow = resp[0].list[5].main.temp_min
 let secondWind = resp[0].list[5].wind.speed
 console.log("Hell yeah Billy Joel reference")
 let secondHumidity = resp[0].list[5].main.humidity
+let thirdTemp = resp[0].list[13].main.temp
+let thirdHigh = resp[0].list[13].main.temp_max
+let thirdLow = resp[0].list[13].main.temp_min
+let thirdWind = resp[0].list[13].wind.speed
+let thirdHumidity = resp[0].list[13].main.humidity
+let fourthTemp = resp[0].list[21].main.temp
+let fourthHigh = resp[0].list[21].main.temp_max
+let fourthLow = resp[0].list[21].main.temp_min
+let fourthWind = resp[0].list[21].wind.speed
+let fourthHumidity = resp[0].list[21].main.humidity
+let fifthTemp = resp[0].list[29].main.temp
+let fifthHigh = resp[0].list[29].main.temp_max
+let fifthLow = resp[0].list[29].main.temp_min
+let fifthWind = resp[0].list[29].wind.speed
+let fifthHumidity = resp[0].list[29].main.humidity
 
 //var for the element IDs
 const currentTemp = document.getElementById('current-temp');
@@ -81,7 +94,20 @@ const currentHumidity = document.getElementById('current-humidity');
 const dayTwoTemp = document.getElementById('second-temp');
 const dayTwoHigh = document.getElementById('second-high-low');
 const dayTwoWind = document.getElementById('second-wind');
-const dayTwoHumidity = document.getElementById("second-humidity");
+const dayTwoHumidity = document.getElementById('second-humidity');
+const dayThreeTemp = document.getElementById('third-temp');
+const dayThreeHigh = document.getElementById('third-high-low');
+const dayThreeWind = document.getElementById('third-wind');
+const dayThreeHumidity = document.getElementById('third-humidity');
+const dayFourTemp = document.getElementById('fourth-temp');
+const dayFourHigh = document.getElementById('fourth-high-low');
+const dayFourWind = document.getElementById('fourth-wind');
+const dayFourHumidity = document.getElementById('fourth-humidity');
+const dayFiveTemp = document.getElementById('fifth-temp');
+const dayFiveHigh = document.getElementById('fifth-high-low');
+const dayFiveWind = document.getElementById('fifth-wind');
+const dayFiveHumidity = document.getElementById('fifth-humidity');
+
 
 //putting data on the page
 currentTemp.innerHTML = "Current Temp: " + temp;
@@ -92,6 +118,20 @@ dayTwoTemp.innerHTML = "Temp: " + secondTemp;
 dayTwoHigh.innerHTML = "High Temp: " + secondHigh + " Low Temp: " + secondLow;
 dayTwoWind.innerHTML = "Wind: " + secondWind + "MPH"
 dayTwoHumidity.innerHTML = "Humidity: " + secondHumidity + "%" 
+dayThreeTemp.innerHTML = "Temp: " + thirdTemp;
+dayThreeHigh.innerHTML = "High Temp: " + thirdHigh + " Low Temp: " + thirdLow;
+dayThreeWind.innerHTML = "Wind: " + thirdWind + "MPH"
+dayThreeHumidity.innerHTML = "Humidity: " + thirdHumidity + "%" 
+dayFourTemp.innerHTML = "Temp: " + fourthTemp;
+dayFourHigh.innerHTML = "High Temp: " + fourthHigh + " Low Temp: " + fourthLow;
+dayFourWind.innerHTML = "Wind: " + fourthWind + "MPH"
+dayFourHumidity.innerHTML = "Humidity: " + fourthHumidity + "%"
+dayFiveTemp.innerHTML = "Temp: " + fifthTemp;
+dayFiveHigh.innerHTML = "High Temp: " + fifthHigh + " Low Temp: " + fourthLow;
+dayFiveWind.innerHTML = "Wind: " + fifthWind + "MPH"
+dayFiveHumidity.innerHTML = "Humidity: " + fifthHumidity + "%"
+
+//learn how to push into arrays
 
       // row.innerHTML = 
   //     forecast.map((day, idx) => {
